e0de9d402da1380eed39284d3d85aea0504ea919
Alex Griffiths <67096118+alexmgriffiths@users.noreply.github.com>
2020-09-29 02:22:54 -0500

    Update tcpproxy.sh

--- tcpproxy.sh
+++ tcpproxy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-IPTABLES=/sbin/iptables
+IPTABLES=$(which iptables)
 echo 1 > /proc/sys/net/ipv4/ip_forward
-$IPTABLES -t nat -F
 $IPTABLES -t nat -A PREROUTING -p tcp --dport $1 -j DNAT --to-destination $2:$3
 $IPTABLES -t nat -A POSTROUTING -j MASQUERADE
